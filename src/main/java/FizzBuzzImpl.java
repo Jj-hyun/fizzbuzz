8e156db96d88f57b378537424615473e5f8e7512
sanghee0820 <lsh277604@gmail.com>
2024-06-04 20:06:50 +0900

    feat: FizzBuzz Initialization

---
+++ src/main/java/FizzBuzzImpl.java
@@ -0,0 +1,18 @@
+public class FizzBuzzImpl implements Fizzbuzz{
+
+    @Override
+    public String checkFizzBuzz(int input) {
+        return null;
+    }
+
+    @Override
+    public boolean checkFizz(int input) {
+        return false;
+    }
+
+    @Override
+    public boolean checkBuzz(int input) {
+        return false;
+    }
+
+}
---
+++ src/main/java/Fizzbuzz.java
@@ -0,0 +1,7 @@
+public interface Fizzbuzz {
+
+    String checkFizzBuzz(int input);
+    boolean checkFizz(int input);
+    boolean checkBuzz(int input);
+
+}
