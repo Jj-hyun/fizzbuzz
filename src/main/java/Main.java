87fe0ea62d14fcf058a5f56adbfcdd5c4aa22cbd
hjinshin <gudwls818@gmail.com>
2024-06-04 20:23:14 +0900

    feat: Main Class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Fizzbuzz fizzbuzz = new FizzBuzzImpl();
+        Scanner scanner = new Scanner(System.in);
 
+        int N = scanner.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            String result = fizzbuzz.checkFizzBuzz(i);
+            System.out.println(result);
+        }
     }
 }
