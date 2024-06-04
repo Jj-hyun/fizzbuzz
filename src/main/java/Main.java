502494ee2f5d8b63c8fce0a8b8b248660373472f
hjinshin <gudwls818@gmail.com>
2024-06-04 20:36:39 +0900

    refactor: scanner close

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,13 @@ public class Main {
         Fizzbuzz fizzbuzz = new FizzBuzzImpl();
         Scanner scanner = new Scanner(System.in);
 
-        int N = scanner.nextInt();
+        int input = scanner.nextInt();
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= input; i++) {
             String result = fizzbuzz.checkFizzBuzz(i);
             System.out.println(result);
         }
+
+        scanner.close();
     }
 }
